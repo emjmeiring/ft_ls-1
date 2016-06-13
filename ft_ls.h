efc6200136be2b76088dc786fde5e37172c030d4
Siyanda-Mzam <dot.netntoz@gmail.com>
2016-06-13 23:39:51 +0200

    Work in progress...

---
+++ ft_ls.h
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: simzam <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/06/13 08:58:44 by simzam            #+#    #+#             */
+/*   Updated: 2016/06/13 09:53:19 by simzam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LS_H
+#define FT_LS_H
+#define STD_SIZ 	sizeof(struct dirent *) * 4
+#define CONTENT 	(void*)reader->open.read
+#define CMP_FILES	((struct dirent *)reader->store.content)->d_name, \
+				((struct dirent *)(reader->store.next->content)->d_name)
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+typedef struct		s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}					t_list;
+
+typedef struct		s_dir_files_opener
+{
+	DIR				*dirp;
+	struct dirent	*read;
+}					t_opener;
+
+typedef struct		s_dir_files_reader
+{
+	t_opener		open;
+	t_list			*store;
+	struct stat		buf;
+}					t_reader;
+
+void				parse(int argc, char **argv, t_reader *reader);
+void				sort(t_reader *reader);
+void				or_curr_dir(t_reader *reader, char *fname);
+void				read_all(t_reader *reader, char *fname);
+void				read_all_nhid(t_reader *reader, char *fname);
+#endif
